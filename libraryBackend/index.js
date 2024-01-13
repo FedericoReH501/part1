d4aa9d5f2578d73652b3f6148b078217ccabed65
Federico Re <federico.re1996@gmail.com>
2024-01-13 15:03:20 +0300

    ok

--- libraryBackend/index.js
+++ libraryBackend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
     authorCount: Int
     bookCount: Int
     allBook(author:String,genre:String): [Book!]
-    allAuthor: [Author!]
+    allAuthor: [Author]
   }
 
   type Mutation{
@@ -125,6 +125,11 @@ const typeDefs = `
       author:String!
       published:Int!
       genres:[String]): Book
+
+      editAuthor(
+        name:String
+        born:Int
+        ): Author
   }
 `
 
@@ -151,20 +156,25 @@ const resolvers = {
   },
   Mutation: {
     addBook: (root, args) => {
-      console.log(authors.length)
       if (authors.filter((a) => a.name === args.author).length === 0) {
         const author = { name: args.author, id: uuid() }
 
-        console.log("Added!!")
         authors = authors.concat(author)
-        console.log(authors)
-        console.log()
       }
-      console.log(authors.length)
       const book = { ...args, id: uuid() }
       books.concat(book)
-      console.log("book", book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.born }
+      console.log("args born", args.born)
+      authors = authors.map((a) => (a.id === author.id ? updatedAuthor : a))
+      console.log(authors)
+      return updatedAuthor
     },
   },
 }
